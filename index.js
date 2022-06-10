db59dd3847c125837b1c8e952d5abddeb7cb5ec3
Ngaicode <mjombakevin376@gmail.com>
2022-06-11 00:07:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+"use strict";
+const eventAnticipated = document.querySelector("input");
+
 function addingEventListener() {
+  eventAnticipated.addEventListener("click", function (event) {
+    return "i have been pressed";
+  });
 }
